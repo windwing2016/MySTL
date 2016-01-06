dc5738ab9584df65cffaafe32cf5cdff934e6ff4
windwing <78356388@qq.com>
2016-01-06 14:59:00 +0800

    master

---
+++ MySTL/include/stl_alloc.h
@@ -0,0 +1,23 @@
+#ifndef __SGL__STL_INTERNAL_ALLOC_H
+#define __SGL__STL_INTERNAL_ALLOC_H
+
+//---------------------------------
+//sun编译器对private限制过多 对其开放权限
+#ifdef __SUNPRO_CC
+#  define __PRIVATE  public
+#else
+#  define __PRIVATE private
+#endif
+
+//--------------------------------------
+
+
+
+
+
+
+
+
+
+
+#endif // __SGL__STL_INTERNAL_ALLOC_H
